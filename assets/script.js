40178eacde747a64920ca53c41b85387e4f982aa
Justin_Anguiano <justin.anguiano@cox.net>
2022-10-26 12:05:38 -0500

    JS updates

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,30 @@
 var topics = ['About', 'Work Experience', 'Contact Info', 'Interests'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if (topic === 'About') {
-  console.log("Let's look at my About!");
-} else if (topic === 'Work Experience!') {
-  console.log("Let's look at my Work Experience!");
-} else if (topic === 'Contact Info') {
-  console.log("Let's look at my Contact Info!");
-} else if (topic === 'Interests') {
-  console.log("Let's look at my Interests!");
-} else {
-  console.log('Can you please try again?');
+function selectTopic() {
+    if (randomTopic === 'About') {
+    console.log("Let's look at my About!");
+    } else if (randomTopic === 'Work Experience!') {
+    console.log("Let's look at my Work Experience!");
+    } else if (randomTopic=== 'Contact Info') {
+    console.log("Let's look at my Contact Info!");
+    } else if (randomTopic === 'Interests') {
+    console.log("Let's look at my Interests!");
+    } else {
+    console.log("Would you mind trying again please?");
+    }
 }
-*/
+console.log("Here is what I'd like to share!");
+listTopics();
+
+console.log('Which topic should we look at first?');
+selectTopic();
+
+
 
