a4319b34080d0a20686db036397617b979e466f2
Justin_Anguiano <justin.anguiano@cox.net>
2022-10-26 10:07:13 -0500

    "JS console.log added"

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var topic = "About";
+
+if (topic === 'About') {
+  console.log("Let's look at my About!");
+} else if (topic === 'CSS') {
+  console.log("Let's look at my Work Experience!");
+} else if (topic === 'Contact Info') {
+  console.log("Let's look at my Contact Info!");
+} else if (topic === 'Interests') {
+  console.log("Let's look at my Interests!");
+} else {
+  console.log('Can you please try again?');
+}
